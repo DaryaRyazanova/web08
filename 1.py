9852a191f3111ab454679c81cb2f1b79115d825b
darya ryazanova <d.riazanova@mail.ru>
2024-02-09 20:18:08 +0300

    task3

--- 1.py
+++ 1.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/training/<prof>')
-def index(prof):
-    return render_template('training.html', title=prof, prof=prof)
+@app.route('/list_prof/<list_>')
+def index(list_):
+    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                   'инженер по терраформированию', 'климатолог',
+                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                   'штурман', 'пилот дронов']
+    return render_template('list_prof.html', title=list_, list_=list_, professions=professions)
 
 
 if __name__ == '__main__':
